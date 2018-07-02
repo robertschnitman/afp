4b5783b752d68248712838af8139a0d6f434ac67
Robert Schnitman <robertschnitman@gmail.com>
2018-07-01 19:22:03 -0500

    Add files via upload

---
+++ R/mapfilter.R
@@ -0,0 +1,39 @@
+#' Apply function over collection and then filter it with a predicate function
+#'
+#' @param f A function to apply to the collection.
+#' @param p Predicate function to filter the data. See \code{\link{Filter}}.
+#' @param x A collection, such as a a list, matrix, or data frame.
+#' @param ... Arguments passed to \code{\link{mapply}}.
+#' 
+#' @details Effectively, this function calls \code{mapply(..., SIMPLIFY = FALSE)} and \code{Filter}.
+#' 
+#' @examples
+#' # 1. Obtain the squared even elements from a vector.
+#' A       <- 1:50
+#' is.even <- function(x) x %% 2 == 0
+#' output1 <- mapfilter(function(x) x^2, is.even, A)
+#' output1
+#' 
+#' # 2. For each dataset, find the cubed odd elements.
+#' l       <- list(mc = mtcars, aq = airquality, lcs = LifeCycleSavings)
+#' is.odd  <- function(x) x %% 2 == 1
+#' mf.odd  <- function(x) mapfilter(function(y) y^3, is.odd, x)
+#' output2 <- telecast(mf.odd, l)
+#' output2 # list of lists.
+#'
+#' @seealso \url{https://github.com/robertschnitman/afp}, \code{\link{Map}}, \code{\link{Filter}}, \code{\link{telecast}}
+
+
+mapfilter <- function(f, p, x, ...) {
+  
+  # 1. Type-check inputs
+  f <- match.fun(f)
+  p <- match.fun(p)
+  
+  # 2. Map THEN filter.
+  output <- Filter(p, mapply(f, x, ...))
+  
+  # 3. Output can be a vector or list, depending on the SIMPLIFY input.
+  output
+  
+}
