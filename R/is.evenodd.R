36d5a216054370b86b17eca66790302ebbfc78f3
Robert Schnitman <robertschnitman@gmail.com>
2018-07-18 22:09:36 -0500

    More examples; vectorize
    1. Show more advanced uses of functions to highlight benefits.
    
    2. Vectorize functions--they're not like "is.numeric()"; treat like "mtcars$gear > 4".

--- R/is.evenodd.R
+++ R/is.evenodd.R
@@ -6,8 +6,9 @@
 #' 
 #' @examples
 #' is.even(1)
+#' sapply(mtcars$gear, is.even)
 #' is.odd(2)
-#' is.even(c(1, 2))
+#' sapply(mtcars$gear, is.odd)
 #' 
 #' @seealso \url{https://github.com/robertschnitman/afp}; `iseven()` and `isodd` in \url{https://docs.julialang.org/en/release-0.4/stdlib/numbers/}
 #' @name is.evenodd
@@ -15,22 +16,10 @@ NULL
 #> NULL
 
 #' @rdname is.evenodd
-is.even <- function(x) {
-  
-  stopifnot(length(x) == 1)
-  
-  x %% 2 == 0
-  
-}
+is.even <- function(x) x %% 2 == 0
 
 #' @rdname is.evenodd
-is.odd <- function(x) {
-  
-  stopifnot(length(x) == 1)
-  
-  x %% 2 == 1
-  
-}
+is.odd <- function(x) x %% 2 == 1
   
   
   
