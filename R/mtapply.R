6f52b31fa696b1cea55caac151053ddbc12f0964
Robert Schnitman <robertschnitman@gmail.com>
2018-09-30 02:31:42 -0500

    Make inputs be consistent with tapply().
    The purpose of mtapply() is to account for situations in which tapply() breaks due to uneven lengths between X and the INDEX. As such, using the same inputs leads to less confusion during use.

--- R/mtapply.R
+++ R/mtapply.R
@@ -2,9 +2,9 @@
 #' 
 #' @description Apply a function over an array by a list of indices. Multivariate version of \code{\link{tapply}}.
 #' 
-#' @param f Function to apply. See \code{\link{tapply}}.
-#' @param x Object where \code{\link{split}} is applicable. See \code{\link{tapply}}.
-#' @param index List of indices. See \code{\link{tapply}} for univariate case.
+#' @param X Object where \code{\link{split}} is applicable. See \code{\link{tapply}}.
+#' @param INDEX List of indices. See \code{\link{tapply}} for univariate case.
+#' @param FUN Function to apply. See \code{\link{tapply}}.
 #' @param ... Parameters passed to \code{\link{mapply}}.
 #' 
 #' @return List.
@@ -19,21 +19,21 @@
 #' 
 #' @seealso \url{https://github.com/robertschnitman/afp}, \code{\link{tapply}}, \code{\link{mapply}}, \code{\link{dot}},
 
-mtapply <- function(f = NULL, x, index, ...) {
+mtapply <- function(X, INDEX, FUN = NULL, ...) {
   
   # 1. Type-check inputs.
-  f    <- match.fun(f)
+  FUN    <- match.fun(FUN)
   
-  if (!is.list(x)) {x <- as.list(X)}
-  if (!is.list(index)) {index <- as.list(index)}
+  if (!is.list(X)) {X <- as.list(X)}
+  if (!is.list(INDEX)) {INDEX <- as.list(INDEX)}
   
   # 2. Generalize tapply for the multivariate case.
-  tapfun <- function(a, b) tapply(a, b, f) # Writing the tapply() on this line makes the output-assignment line more readable than otherwise.
+  tapfun <- function(a, b) tapply(a, b, FUN) # Writing the tapply() on this line makes the output-assignment line more readable than otherwise.
   
-  output <- mapply(tapfun, X = x, INDEX = index, ...)
+  output <- mapply(tapfun, X, INDEX, ...)
   
   # 3. Edit names of the output to differentiate/identify the target and associated index.
-  names(output) <- paste0(names(output), '_by_', names(index))
+  names(output) <- paste0(names(output), '_by_', names(INDEX))
   
   output # Output = list. See tapply documentation.
   
