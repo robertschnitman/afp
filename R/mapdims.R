402127c150a339f3bc0cb368504435d9347b907e
Robert Schnitman <robertschnitman@gmail.com>
2019-02-23 19:13:42 -0600

    New column- and row-wise functions
    Abbreviations for `apply(x, 1, f)` and `apply(x, 2, f)` (row-wise and column-wise respectively).

--- R/mapdims.R
+++ R/mapdims.R
@@ -1,22 +1,28 @@
 #' Map over matrix dimensions separately in a single function call
 #' 
-#' @description Apply function over dimensions and save the column- and row-wise results separately in a list.
+#' @description Map over matrix dimensions separately in a single function call 
 #' 
 #' @usage mapdims(f, x, ...)
+#' mapc(f, x)
+#' mapr(f, x)
 #'
 #' @param f A function to apply to the 2D dataset.
 #' @param x A 2D dataset, such as a matrix or data frame.
 #' @param ... Arguments passed to the \code{f}.
 #' 
-#' @return List. Two elements: \code{rowwise} for the row-wise computations, and \code{colwise} for the column-specific results.
+#' @return List for mapdims(). Two elements: \code{rowwise} for the row-wise computations, and \code{colwise} for the column-specific results. Output is array for `mapc()` and `mapr()`
+#' 
+#' @details Apply function over dimensions and save the column- and row-wise results separately in a list with `mapdims()`; convenient `apply()` abbreviations with `mapc()` and `mapr()`.
 #' 
 #' @examples
 #' mapdims(median, mtcars)
-#' 
 #' mapdims(mean, airquality, na.rm = TRUE)
+#' mapc(median, mtcars)
+#' mapr(median, mtcars) 
 #'
 #' @seealso \url{https://github.com/robertschnitman/afp}, \code{\link{apply}}
 
+#' @rdname mapdims
 mapdims <- function(f, x, ...) {
   
   ## 1. Type-check data object x--must be 2D due to apply().
@@ -31,3 +37,9 @@ mapdims <- function(f, x, ...) {
   output
   
 }
+
+#' @rdname mapc
+mapc <- function(f, x, ...) apply(x, 2, f, ...)
+
+#' @rdname mapr
+mapr <- function(f, x, ...) apply(x, 1, f, ...)
