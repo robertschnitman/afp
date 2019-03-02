ef47514710ecde2a37ccb3c75d78f0539eb104e6
Robert Schnitman <robertschnitman@gmail.com>
2019-03-02 12:24:58 -0600

    Clarify intent; names should be appropriate
    1. Clarify the intent of the functions so the reader knows the benefits.
    
    2. The word "anagram" in the eponymous function implies that the reordering resembles another legitimate word when it randomly changes the order instead; thus, "jumble" is more appropriate.

--- R/mapchr.R
+++ R/mapchr.R
@@ -4,20 +4,20 @@
 #'
 #' @usage mapchr(f, x)
 #' reverse(x)
-#' anagram(x)
+#' jumble(x)
 #' 
 #' @param f Function to apply to the character vector `x`.
 #' @param x Character vector.
 #' 
 #' @return Vector.
 #' 
-#' @details `mapchr()` is a general functional for altering character vectors: apply any function to each of its elements. The function `reverse()` reverses the order of characters for each element. `anagram(x)` randomly changes the order of the characters in every element. Similar to `map_chr()` from the `purrr` library with the exception that the former only accepts character vectors as the data input.
+#' @details `mapchr()` is a general functional for altering character vectors: apply any function to each of its elements. The function `reverse()` reverses the order of characters for each element. `jumble(x)` randomly changes the order of the characters in every element. Similar to `map_chr()` from the `purrr` library with the exception that the former only accepts character vectors as the data input. These functions are useful when manipulating the arrangement of the characters is desired.
 #' 
 #' @examples
 #' rn_mc <- rownames(mtcars)
 #' mapchr(function(x) paste0(x, collapse = '|'), rn_mc)
 #' reverse(rn_mc)
-#' anagram(rn_mc)
+#' jumble(rn_mc)
 #' 
 #' @seealso \url{https://github.com/robertschnitman/afp}, \code{\link{lapply}}, 
 #' \code{reverse} from Julia: \url{https://docs.julialang.org/en/v1/base/strings/#Base.reverse-Tuple{Union{SubString{String},%20String}}}
@@ -50,5 +50,5 @@ mapchr <- function(f, x) {
 #' @rdname reverse
 reverse <- function(x) mapchr(rev, x)
 
-#' @rdname anagram
-anagram <- function(x) mapchr(sample, x)
+#' @rdname jumble
+jumble <- function(x) mapchr(sample, x)
