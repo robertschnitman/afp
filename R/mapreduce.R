93d7fa301cf12c5721299f8c8e76d708686cd1eb
Robert Schnitman <robertschnitman@gmail.com>
2019-08-31 23:41:43 -0500

    Output type/class

--- R/mapreduce.R
+++ R/mapreduce.R
@@ -49,7 +49,7 @@ mapreduce <- function(f, o, x, y = NULL, ...) {
   # 2. Map the function f to collection x and iteratively reduce the mapping by the binary operator o.
   output <- Reduce(o, Map(f, x, MoreArgs = y), ...)
   
-  # 3. Output should be a matrix or data frame.
+  # 3. Output should be an array or data frame.
   output
   
 }
