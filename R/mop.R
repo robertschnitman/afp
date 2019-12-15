02419bf13bfb7824971e6bbca81f767c2672ac71
Robert Schnitman <robertschnitman@gmail.com>
2019-12-15 17:39:29 -0600

    New function: mop_div
    WHY: Further simplification.

--- R/mop.R
+++ R/mop.R
@@ -2,7 +2,7 @@
 #' 
 #' @description Based on \code{\link{sweep}}, operate on an array by a summary statistic function.
 #' 
-#' @usage mop(x, m, s, f, ...), smop(x, s, f, ...)
+#' @usage mop(x, m, s, f, ...), smop(x, s, f, ...), mop_div(x, s, ...)
 #'
 #' @param x An array.
 #' @param m Margin. 1 for rows, 2 for columns.
@@ -10,7 +10,7 @@
 #' @param f A function to be "swept" or "mopped" out, typically a binary operator.
 #' @param ... Arguments passed to \code{\link{sweep}}.
 #' 
-#' @details Essentially, \code{mop} is a wrapper for \code{sweep(x, MARGIN, apply(...), FUN)}. Useful for indexing variables by their means, for example, so that the magnitude of a value relative to its average is known. The function \code{smop} simplifies \code{mop} by operating only on columns.
+#' @details Essentially, \code{mop} is a wrapper for \code{sweep(x, MARGIN, apply(...), FUN)}. Useful for indexing variables by their means, for example, so that the magnitude of a value relative to its average is known. The function \code{smop} simplifies \code{mop} by operating only on columns. The function \code{mop_div} simplifies \code{mop} further by operating only on columns and assuming \code{f} to be \code{`/`}.
 #' 
 #' @examples
 #' mop(mtcars, 2, mean, `/`) # == sweep(mtcars, 2, apply(mtcars, 2, mean), `/`)
@@ -41,3 +41,5 @@ mop <- function(x, m, s, f, ...) {
 }
 
 smop <- function(x, s, f, ...) mop(x, 2, s, f, ...)
+
+mop_div <- function(x, s, ...) smop(x, s, `/`, ...)
